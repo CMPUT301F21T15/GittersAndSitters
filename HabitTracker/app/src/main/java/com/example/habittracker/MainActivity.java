c1bf950904f15dca3be7f5638261411e802df6f0
Aaron Skiba <askiba@ualberta.ca>
2021-10-28 15:04:50 -0600

    Update MainActivity.java
    Instantiated a User and added a Habit to their habit list. Also added button logic to navigate User HabitList to ActivityAllHabits

--- HabitTracker/app/src/main/java/com/example/habittracker/MainActivity.java
+++ HabitTracker/app/src/main/java/com/example/habittracker/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.habittracker;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import androidx.annotation.RequiresApi;
@@ -15,10 +18,11 @@ import java.util.Date;
 public class MainActivity extends AppCompatActivity {
 
     // Declare the variables so that you will be able to reference it later.
-    ListView cityList;
-    ArrayAdapter<com.example.habittracker.Habit> cityAdapter;
-    ArrayList<com.example.habittracker.Habit> cityDataList;
+    ListView todayHabitsList;
+    ArrayAdapter<Habit> cityAdapter;
+    ArrayList<Habit> cityDataList;
     ArrayList<DayOfWeek> weekdays;
+    User user;
 
     @RequiresApi(api = Build.VERSION_CODES.O)           // api required to implement DayOfWeek
     @Override
@@ -26,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        cityList = findViewById(R.id.city_list);
+        todayHabitsList = findViewById(R.id.today_habits_list);
 
         /**
          * Test creating a new Habit
@@ -36,24 +40,49 @@ public class MainActivity extends AppCompatActivity {
          *         habitReason;
          */
 
-        String habitName = "Running";
+        String habitName = "Run 2km";
         weekdays = new ArrayList<>();
         weekdays.add(DayOfWeek.MONDAY);
         weekdays.add(DayOfWeek.WEDNESDAY);
         weekdays.add(DayOfWeek.FRIDAY);
-        String habitReason = "Get in shape";
+        String habitReason = "Stay in shape";
 
+        // Instantiate new habit;
         Habit habit = new Habit(habitName, weekdays, habitReason);
+        // Create new User and add Habit
+        user = new User(new ArrayList<>());
+        user.addHabit(habit);
+
 
         cityDataList = new ArrayList<>();
         cityDataList.add(habit);
 
 
-        cityAdapter = new HabitCustomList(this, cityDataList);
+        cityAdapter = new HabitCustomList(this, user.getTodayHabits());
 
-        cityList.setAdapter(cityAdapter);
+        todayHabitsList.setAdapter(cityAdapter);
 
+        final Button allHabitsButton = findViewById(R.id.all_habits_button);
+        allHabitsButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AllHabitsActivity.class);
+                //ArrayList<Habit> allHabits = user.getAllUserHabits();
+                intent.putExtra("user", user);
+                startActivity(intent);
+            }
+        });
     }
-
-
 }
+
+/**
+ * / THIS METHOD MAY NOT BE NECESSARY
+ * / Retains ListView when returning to MainActivity
+ @Override
+ public boolean onOptionsItemSelected(MenuItem item) {
+ if(item.getItemId() == android.R.id.home) {
+ onBackPressed();
+ return true;
+ }
+ return super.onOptionsItemSelected(item);
+ }
+ */
