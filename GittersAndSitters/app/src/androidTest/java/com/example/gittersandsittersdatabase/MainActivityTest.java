f4007e823efacc651beee2ce2b44b0e9ccafcb91
TraianOglinzanu <oglinzan@ualberta.ca>
2021-11-04 16:33:15 -0600

    some quick tests for activity main

--- GittersAndSitters/app/src/androidTest/java/com/example/gittersandsittersdatabase/MainActivityTest.java
+++ GittersAndSitters/app/src/androidTest/java/com/example/gittersandsittersdatabase/MainActivityTest.java
@@ -10,11 +10,11 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
+import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withSpinnerText;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static java.util.regex.Pattern.matches;
 
 import android.content.Intent;
 import android.view.View;
@@ -24,45 +24,63 @@ public class MainActivityTest {
     @Rule
     public ActivityTestRule<MainActivity> mainActivityActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
-//    private String fakeEmail = "abcd@gmail.com";
-    private String fakePassword = "1379246";
+    //Build the result by setting up an email
+
+    String fakeEmail = "abc@gmail.com";
 
     @Before
     public void setUp() throws Exception {
     }
 
     @Test
-    public void testUserInputScenario() {
-
-        //Build the result
-
-        Intent resultData = new Intent();
-        String fakeEmail = "abc@gmail.com";
-        resultData.putExtra("email", fakeEmail);
+    public void testUserEmailScenario() {
 
         //Input some text in the email field
         //Launch activity to be returned and displayed
+
         Espresso.onView(withId(R.id.login_emailAddress)).perform(typeText(fakeEmail));
 
         //Assert that the data we set up above is shown
-        Espresso.onView(withId(R.id.login_emailAddress)).check(matches(withText(fakeEmail)));
+
+        Espresso.onView(withId(R.id.activity_main_id)).check(matches(withText(fakeEmail)));
 
     }
 
     @Test
     public void testPasswordScenario(){
 
+        //Build the result
+
+        Intent result = new Intent();
+        String fakePassword = "fakePassword";
+        result.putExtra("password", fakePassword);
+
        //Input some text in the email field
        //Launch activity to be returned and displayed
+
        Espresso.onView(withId(R.id.login_password)).perform(typeText(fakePassword));
 
        //Assert that the data we set up above is returned
-       Espresso.onView(withId(R.id.login_password)).check(matches(withText(fakePassword)));
+
+       Espresso.onView(withId(R.id.activity_main_id)).check(matches(withText(fakePassword)));
+
+    }
+
+    @Test
+    public void testLoginButton(){
+        //Click on log in button
+
+        Espresso.onView(withId(R.id.login_button)).perform(click());
+
+        //Test to see if user is logged in
+
+        Espresso.onView(withId(R.id.activity_main_id)).check(matches(isDisplayed()));
 
     }
 
 
     @After
     public void tearDown() throws Exception {
+
     }
 }
